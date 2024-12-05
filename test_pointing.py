a9aecd44b943d90615c012ad03c9c1a3477c5bda
Namhoon Cho <59905641+Namhoon-Cho@users.noreply.github.com>
2024-12-05 17:46:15 +0900

    test_pointing.py 파일 삭제
    불필요한 파일 삭제

--- test_pointing.py
+++
@@ -1,5 +0,0 @@
-from attentionme import add_pointed_person
-
-image_path = "C:\\Users\\nhcho\\Downloads\\71086_220513_3029.png"
-output_path = "C:\\Users\\nhcho\\Downloads\\pointed.png"
-add_pointed_person(image_path, output_path)
