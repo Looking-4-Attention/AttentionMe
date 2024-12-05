39b7fbd23514465086343c72a8b74067f1eec37c
wwwwje2008 <wwwwje2008@gmail.com>
2024-12-05 18:27:04 +0900

    __init__.py 수정

--- attentionme/__init__.py
+++ attentionme/__init__.py
@@ -1,3 +1,6 @@
 from .segmentation import segment_person
 from .adjustBrightness import adjustBrightness
-__all__ = ["segment_person","adjustBrightness"]
+from .crop import crop
+from .remove_background import remove_background
+
+__all__ = ["segment_person", "adjustBrightness", "adjustBrightness", "crop", "remove_background"]
